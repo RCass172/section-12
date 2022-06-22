65d45b067f723cb2273a9e731de9fb8e6fa51f91
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-22 12:07:28 +0100

    Add binary search practice

--- src/main/java/com/zinkworks/Main.java
+++ src/main/java/com/zinkworks/Main.java
@@ -1,7 +1,19 @@
-package main.java.com.zinkworks;
+package com.zinkworks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+//        theatre.getSeats();
+        if(theatre.reserveSeat("B13")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
     }
 }
